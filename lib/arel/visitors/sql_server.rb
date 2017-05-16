96531f75d2127a84571847fadbcf6c06012a06e2
Félix Bellanger <felix.bellanger@gmail.com>
2017-05-16 16:56:21 +0200

    Using default arel

--- lib/arel/visitors/sql_server.rb
+++ lib/arel/visitors/sql_server.rb
@@ -2,212 +2,125 @@ require 'arel/visitors/compat'
 
 module Arel
   module Visitors
-    class SQLServerNG < Arel::Visitors::ToSql
+    class SQLServer < Arel::Visitors::ToSql
 
-      OFFSET = " OFFSET "
-      ROWS = " ROWS"
-      FETCH = " FETCH NEXT "
-      FETCH0 = " FETCH FIRST (SELECT 0) "
-      ROWS_ONLY = " ROWS ONLY"
+      RowNumber = Struct.new :children
 
-
-      private
-
-      # SQLServer ToSql/Visitor (Overides)
-
-      def visit_Arel_Nodes_BindParam o, collector
-        collector.add_bind(o) { |i| "@#{i-1}" }
-      end
-
-      def visit_Arel_Nodes_Bin o, collector
-        visit o.expr, collector
-        if o.expr.val.is_a? Numeric
-          collector
-        else
-          collector << " #{::ArJdbc::MSSQL.cs_equality_operator} "
-        end
-      end
-
-      def visit_Arel_Nodes_UpdateStatement(o, a)
-        if o.orders.any? && o.limit.nil?
-          o.limit = Nodes::Limit.new(9_223_372_036_854_775_807)
-        end
+      def initialize(*)
+        @primary_keys = {}
         super
       end
 
-      def visit_Arel_Nodes_Lock o, collector
-        o.expr = Arel.sql('WITH(UPDLOCK)') if o.expr.to_s =~ /FOR UPDATE/
-        collector << SPACE
-        visit o.expr, collector
+      private
+
+      # `top` wouldn't really work here. I.e. User.select("distinct first_name").limit(10) would generate
+      # "select top 10 distinct first_name from users", which is invalid query! it should be
+      # "select distinct top 10 first_name from users"
+      def visit_Arel_Nodes_Top o
+        ""
       end
 
-      def visit_Arel_Nodes_Offset o, collector
-        collector << OFFSET
-        visit o.expr, collector
-        collector << ROWS
-      end
-
-      def visit_Arel_Nodes_Limit o, collector
-        if node_value(o) == 0
-          collector << FETCH0
-          collector << ROWS_ONLY
-        else
-          collector << FETCH
-          visit o.expr, collector
-          collector << ROWS_ONLY
-        end
+      def visit_Arel_Visitors_MSSQL_RowNumber o, collector
+        collector << "ROW_NUMBER() OVER (ORDER BY "
+        inject_join(o.children, collector, ', ') << ") as _row_num"
       end
 
       def visit_Arel_Nodes_SelectStatement o, collector
-        @select_statement = o
-        distinct_One_As_One_Is_So_Not_Fetch o
-        if o.with
-          collector = visit o.with, collector
-          collector << SPACE
+        if !o.limit && !o.offset
+          return super
         end
-        collector = o.cores.inject(collector) { |c,x|
-          visit_Arel_Nodes_SelectCore(x, c)
+
+        is_select_count = false
+        o.cores.each { |x|
+          core_order_by = row_num_literal determine_order_by(o.orders, x)
+          if select_count? x
+            x.projections = [core_order_by]
+            is_select_count = true
+          else
+            x.projections << core_order_by
+          end
         }
-        collector = visit_Orders_And_Let_Fetch_Happen o, collector
-        collector = visit_Make_Fetch_Happen o, collector
-        collector
-      ensure
-        @select_statement = nil
-      end
 
-      def visit_Arel_Table o, collector
-        # Apparently, o.engine.connection can actually be a different adapter
-        # than sqlserver. Can be removed if fixed in ActiveRecord. See:
-        # github.com/rails-sqlserver/activerecord-sqlserver-adapter/issues/450
-        table_name = if o.engine.connection.respond_to?(:sqlserver?) && o.engine.connection.database_prefix_remote_server?
-          remote_server_table_name(o)
+        if is_select_count
+          # fixme count distinct wouldn't work with limit or offset
+          collector << "SELECT COUNT(1) as count_id FROM ("
+        end
+
+        collector << "SELECT _t.* FROM ("
+        collector = o.cores.inject(collector) { |c,x|
+          visit_Arel_Nodes_SelectCore x, c
+        }
+        collector << ") as _t WHERE #{get_offset_limit_clause(o)}"
+
+        if is_select_count
+          collector << ") AS subquery"
         else
-          quote_table_name(o.name)
+          collector
         end
-        if o.table_alias
-          collector << "#{table_name} #{quote_table_name o.table_alias}"
+      end
+
+      def get_offset_limit_clause o
+        first_row = o.offset ? o.offset.expr.to_i + 1 : 1
+        last_row  = o.limit ? o.limit.expr.to_i - 1 + first_row : nil
+        if last_row
+          " _row_num BETWEEN #{first_row} AND #{last_row}"
         else
-          collector << table_name
+          " _row_num >= #{first_row}"
         end
       end
 
-      def visit_Arel_Nodes_JoinSource o, collector
-        if o.left
-          collector = visit o.left, collector
-          collector = visit_Arel_Nodes_SelectStatement_SQLServer_Lock collector
+      def visit_Arel_Nodes_DeleteStatement o, collector
+        collector << 'DELETE '
+        if o.limit
+          collector << 'TOP ('
+          visit o.limit.expr, collector
+          collector << ') '
         end
-        if o.right.any?
-          collector << " " if o.left
-          collector = inject_join o.right, collector, ' '
-        end
-        collector
-      end
-
-      def visit_Arel_Nodes_OuterJoin o, collector
-        collector << "LEFT OUTER JOIN "
-        collector = visit o.left, collector
-        collector = visit_Arel_Nodes_SelectStatement_SQLServer_Lock collector, space: true
-        collector << " "
-        visit o.right, collector
-      end
-
-      # SQLServer ToSql/Visitor (Additions)
-
-      def visit_Arel_Nodes_SelectStatement_SQLServer_Lock collector, options = {}
-        if select_statement_lock?
-          collector = visit @select_statement.lock, collector
-          collector << SPACE if options[:space]
-        end
-        collector
-      end
-
-      def visit_Orders_And_Let_Fetch_Happen o, collector
-        make_Fetch_Possible_And_Deterministic o
-        unless o.orders.empty?
-          collector << SPACE
-          collector << ORDER_BY
-          len = o.orders.length - 1
-          o.orders.each_with_index { |x, i|
-            collector = visit(x, collector)
-            collector << COMMA unless len == i
-          }
-        end
-        collector
-      end
-
-      def visit_Make_Fetch_Happen o, collector
-        o.offset = Nodes::Offset.new(0) if o.limit && !o.offset
-        collector = visit o.offset, collector if o.offset
-        collector = visit o.limit, collector if o.limit
-        collector
-      end
-
-      # SQLServer Helpers
-
-      def node_value(node)
-        return nil unless node
-        case node.expr
-        when NilClass then nil
-        when Numeric then node.expr
-        when Arel::Nodes::Unary then node.expr.expr
+        collector << 'FROM '
+        collector = visit o.relation, collector
+        if o.wheres.any?
+          collector << ' WHERE '
+          inject_join o.wheres, collector, AND
+        else
+          collector
         end
       end
 
-      def select_statement_lock?
-        @select_statement && @select_statement.lock
-      end
-
-      def make_Fetch_Possible_And_Deterministic o
-        return if o.limit.nil? && o.offset.nil?
-        t = table_From_Statement o
-        pk = primary_Key_From_Table t
-        return unless pk
-        if o.orders.empty?
-          # Prefer deterministic vs a simple `(SELECT NULL)` expr.
-          o.orders = [pk.asc]
+      def determine_order_by orders, x
+        if orders.any?
+          orders
+        elsif x.groups.any?
+          x.groups
+        else
+          pk = find_left_table_pk(x.froms)
+          pk ? [pk] : []
         end
       end
 
-      def distinct_One_As_One_Is_So_Not_Fetch o
-        core = o.cores.first
-        distinct = Nodes::Distinct === core.set_quantifier
-        oneasone = core.projections.all? { |x| x == ActiveRecord::FinderMethods::ONE_AS_ONE }
-        limitone = node_value(o.limit) == 1
-        if distinct && oneasone && limitone && !o.offset
-          core.projections = [Arel.sql("TOP(1) 1 AS [one]")]
-          o.limit = nil
+      def row_num_literal order_by
+        RowNumber.new order_by
+      end
+
+      def select_count? x
+        x.projections.length == 1 && Arel::Nodes::Count === x.projections.first
+      end
+
+      # FIXME raise exception of there is no pk?
+      def find_left_table_pk o
+        if o.kind_of?(Arel::Nodes::Join)
+          find_left_table_pk(o.left)
+        elsif o.instance_of?(Arel::Table)
+          find_primary_key(o)
         end
       end
 
-      def table_From_Statement o
-        core = o.cores.first
-        if Arel::Table === core.from
-          core.from
-        elsif Arel::Nodes::SqlLiteral === core.from
-          Arel::Table.new(core.from)
-        elsif Arel::Nodes::JoinSource === core.source
-          Arel::Nodes::SqlLiteral === core.source.left ? Arel::Table.new(core.source.left, @engine) : core.source.left
+      def find_primary_key(o)
+        @primary_keys[o.name] ||= begin
+          primary_key_name = @connection.primary_key(o.name)
+          # some tables might be without primary key
+          primary_key_name && o[primary_key_name]
         end
-      end
-
-      def primary_Key_From_Table t
-        return unless t
-        return t.primary_key if t.primary_key
-        if engine_pk = t.engine.primary_key
-          pk = t.engine.arel_table[engine_pk]
-          return pk if pk
-        end
-        pk = t.engine.connection.schema_cache.primary_keys(t.engine.table_name)
-        return pk if pk
-        column_name = t.engine.columns.first.try(:name)
-        column_name ? t[column_name] : nil
-      end
-
-      def remote_server_table_name o
-        ActiveRecord::ConnectionAdapters::SQLServer::Utils.extract_identifiers(
-          "#{o.engine.connection.database_prefix}#{o.name}"
-        ).quoted
-      end
+end
 
     end
   end
